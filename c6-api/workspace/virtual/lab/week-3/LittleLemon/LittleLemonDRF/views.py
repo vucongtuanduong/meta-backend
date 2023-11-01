c0a06bd1042c8a6d3e3b9c39ffdb5110938cf3e0
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2023-11-01 11:32:23 +0700

    c6 w2 v1

---
+++ c6-api/workspace/virtual/lab/week-3/LittleLemon/LittleLemonDRF/views.py
@@ -0,0 +1,7 @@
+# from rest_framework import generics
+# from .models import MenuItem, Category
+# from .serializers import MenuItemSerializer, CategorySerializer
+
+# class CategoriesView(generics.ListCreateAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
