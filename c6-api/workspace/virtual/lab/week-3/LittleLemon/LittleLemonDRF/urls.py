0b18404f69f6be4c87cf4da7f3dfe11f4d4dae2d
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2023-11-02 16:05:30 +0700

    c6 w3 v1

--- c6-api/workspace/virtual/lab/week-3/LittleLemon/LittleLemonDRF/urls.py
+++ c6-api/workspace/virtual/lab/week-3/LittleLemon/LittleLemonDRF/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categories', views.CategoriesView.as_view()),
-    path('menu-items', views.MenuItemsView.as_view()),
+    path("menu-items", views.MenuItemsViewSet.as_view({"get": "list"})),
+    path("menu-items/<int:pk>", views.MenuItemsViewSet.as_view({"get": "retrieve"})),
 ]
--- c6-api/workspace/virtual/lab/week-3/LittleLemon/LittleLemonDRF/views.py
+++ c6-api/workspace/virtual/lab/week-3/LittleLemon/LittleLemonDRF/views.py
@@ -1,14 +1,18 @@
-from rest_framework import generics
-from .models import MenuItem, Category
-from .serializers import MenuItemSerializer, CategorySerializer
+from rest_framework.response import Response
+from rest_framework import viewsets
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 
-class CategoriesView(generics.ListCreateAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
 
-class MenuItemsView(generics.ListCreateAPIView):
+class MenuItemsViewSet(viewsets.ModelViewSet):
+    # throttle_classes = [AnonRateThrottle, UserRateThrottle]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    ordering_fields = ['price', 'inventory']
-    filterset_fields = ['price', 'inventory']
-    search_fields = ['title']
+
+    def get_throttles(self):
+        if self.action == "create":
+            throttle_classes = [UserRateThrottle]
+        else:
+            throttle_classes = []
+        return [throttle() for throttle in throttle_classes]
